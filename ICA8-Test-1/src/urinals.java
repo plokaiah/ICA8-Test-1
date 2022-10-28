144f87e0ea8a3ab6dc9e1bb9bb6707121171ab46
Prem Raj Lokaiahgari <103160201+plokaiah@users.noreply.github.com>
2022-10-27 21:02:47 -0700

    Add files via upload

---
+++ ICA8-Test-1/src/urinals.java
@@ -0,0 +1,55 @@
+
+
+import java.lang.*;
+import java.util.*;
+
+public class urinals {
+	public static void main(String[] args)
+	{
+		System.out.println("Enter Your Input Strings: ");
+		getString();
+	}
+	
+	public static void getString()
+	{
+		Scanner s1 =new Scanner(System.in);
+		String k = s1.nextLine();
+		
+		while(!k.equals("-1"))
+		{
+			if(validateString(k))
+			{
+				System.out.println("Success");
+			}
+			else 
+			{
+				System.out.println("Not Success");
+			}
+			k = s1.nextLine();
+		}
+	}
+	
+	public static boolean validateString(String inpStr)
+	{
+		int c=0;
+		int i=0;
+		while(i<inpStr.length())
+		{
+			if(inpStr.charAt(i)=='0'  || inpStr.charAt(i)== '1') {
+				c=0;
+			}
+			else {
+				c=1;
+			}
+			i=i+1;
+		}
+		if(c==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+}
+
---
+++ ICA8-Test-1/src/urinalsTest.java
@@ -0,0 +1,17 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+
+class urinalsTest {
+
+		@Test
+		void test3() {
+			urinals bs = new urinals();
+			String s = "abcd";
+			assertEquals(true,bs.validateString(s));
+			System.out.println("====== PREM RAJ LOKAIAHGARI == TEST ONE EXECUTED =======");  
+		}
+	
+
+}
