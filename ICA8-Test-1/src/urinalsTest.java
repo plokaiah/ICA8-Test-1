a53a8abb31f47e948a2f3cb37b806166a69695de
Prem Raj Lokaiahgari <103160201+plokaiah@users.noreply.github.com>
2022-10-27 21:22:21 -0700

    Add files via upload

--- ICA8-Test-1/src/urinalsTest.java
+++ ICA8-Test-1/src/urinalsTest.java
@@ -12,6 +12,14 @@ class urinalsTest {
 			assertEquals(true,bs.validateString(s));
 			System.out.println("====== PREM RAJ LOKAIAHGARI == TEST ONE EXECUTED AND PASSED =======");  
 		}
-	
+		
+		@Test
+		void testing3() {
+			urinals bs = new urinals();
+			String s = "224";
+			assertEquals(false,bs.validateString(s));
+			System.out.println("====== PREM RAJ LOKAIAHGARI == TEST TWO EXECUTED AND PASSED =======");  
+		}
+		
 
 }
